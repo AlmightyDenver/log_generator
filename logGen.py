a0dd8a7c2bb7f5cc5b0009c64519ebc7dd7ff035
denveralmighty@gmail.com <denveralmighty@gmail.com>
2022-04-18 17:02:00 +0900

    added sleep argument

--- logGen.py
+++ logGen.py
@@ -18,23 +18,8 @@ import argparse
 import numpy
 import socket
 
-def convertLog(log_format, log_dict):
-    message = ''
 
-    if log_format == 'csv':
-        message=str(list(log_dict.values())).strip('[]')
-    elif log_format =='xml':
-        message=dicttoxml(log_dict).decode()
-    elif log_format == 'txt':
-        message=str(log_dict).strip('{|}')
-    elif log_format == 'json':
-        message=str(log_dict)
-    # print(' : ' + message)
-    # print( message)
-    return message
-
-
-def log_gen(time_loc,log_format,time_format, send_log):
+def log_gen(time_loc,log_format,time_format, send_type):
     # create empty dictionary
     mylog = {}
 
@@ -59,7 +44,7 @@ def log_gen(time_loc,log_format,time_format, send_log):
     byt = int(random.gauss(5000,50))
     useragent = numpy.random.choice(ualist,p=[0.5,0.3,0.1,0.05,0.05] )()
     # Create events with more than 10000 characters
-    if (send_log == 'none' and  numpy.random.choice([0,1], p=[0.95, 0.05])):
+    if (send_type == 'none' and  numpy.random.choice([0,1], p=[0.95, 0.05])):
         tenth = 'LOG_TEST message 10000 ABCDEFGHIJKLMNOPQRSTUBWXYZ ' * 200
     else:
         tenth= 'NULL'
@@ -80,6 +65,45 @@ def log_gen(time_loc,log_format,time_format, send_log):
 
     return mylog
 
+def convert_log(log_format, log_dict):
+    message = ''
+    if log_format == 'csv':
+        message=str(list(log_dict.values())).strip('[]')
+    elif log_format =='xml':
+        message=dicttoxml(log_dict).decode()
+    elif log_format == 'txt':
+        message=str(log_dict).strip('{|}')
+    elif log_format == 'json':
+        message=str(log_dict)
+    # print( message)
+    return message
+
+def send_log(type, dest_ip, dest_port, log_format, my_log):
+    # init socket
+    sock = None
+    # Set timeout
+    timeout=5
+    # Set dest address
+    addr = (dest_ip, dest_port)
+    # Message to send
+    message = convert_log(log_format, my_log).encode()
+
+    if type=='tcp':
+        # create socket ?
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(timeout)
+        # Connect
+        sock.connect(addr)
+        # Send Message
+        sock.sendall(message)
+    elif type == 'udp':
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.settimeout(timeout)
+        sock.sendto(message, (dest_ip, dest_port))
+
+    # Close socket
+    sock.close()
+
 
 
 def main():
@@ -88,11 +112,12 @@ def main():
     parser = argparse.ArgumentParser(description='Random Log Generator', add_help=True)
     parser.add_argument('--log_path', '-p', dest='log_path', help='default : ./', default='')
     parser.add_argument('--log_format', '-l', dest='log_format', help='Select log format', choices=['csv','json', 'txt', 'xml'], default='csv' )
-    parser.add_argument('--send_log', '-s', dest='send_log', help='Send Log', choices=['none','tcp', 'udp'], default='none' )
-    parser.add_argument('--time-format', '-tf', dest='time_format', help='Set time format 1 : %%Y-%%m-%%d %%H:%%M, 2 : %%Y/%%m/%%d %%H:%%M:%%S\n', choices=[1, 2], default=1, type=int )
+    parser.add_argument('--send_type', '-s', dest='send_type', help='Send Log', choices=['none','tcp', 'udp'], default='none' )
+    parser.add_argument('--time_format', '-tf', dest='time_format', help='Set time format 1 : %%Y-%%m-%%d %%H:%%M, 2 : %%Y/%%m/%%d %%H:%%M:%%S\n', choices=[1, 2], default=1, type=int )
     parser.add_argument('--time_location', '-t', dest='time_location', help='Setting the location of datetime(0~7).-1 : the end of row', default=0, type=int)
     parser.add_argument('--dest_ip', '-di', dest='dest_ip', help='Dest IP', default='172.20.1.35' )
     parser.add_argument('--dest_port', '-dp', dest='dest_port', help='Dest PORT', default='10514', type=int)
+    parser.add_argument('--sleep', '-slp', dest='sleep_time', help = 'Sleep time', default=5, type=int)
     args = parser.parse_args()
 
     log_path = args.log_path
@@ -100,21 +125,20 @@ def main():
     log_format = args.log_format
     dest_ip = args.dest_ip
     dest_port = args.dest_port
-    send_log = args.send_log
+    send_type = args.send_type
     time_format = '%Y-%m-%d %H:%M:%S' if args.time_format == 1 else '%Y/%m/%d %H:%M:%S'
+    sleep_time = args.sleep_time
 
 
     while True:
-        # execute log_gen functin
-        mylog = log_gen(time_loc,log_format,time_format, send_log )
+        # execute log_gen function
+        mylog = log_gen(time_loc,log_format,time_format, send_type)
         # set file path + file name
-        filename = log_path + datetime.datetime.today().strftime('%m-%d-%Y') + '_Log_Generator.' + log_format
-        # print(log_path)
+        filename = log_path + datetime.datetime.today().strftime('%Y-%m-%d') + '_Log_Generator.' + log_format
+        # print(filename)
 
         # Save as file
-        # Save as CSV
-        
-        if send_log == 'none':
+        if send_type == 'none':
             if log_format == 'csv':
                 # header list
                 header_list=list(mylog.keys())
@@ -142,35 +166,12 @@ def main():
                 with open(filename, 'a+') as f:
                     f.write(mylog)
                     f.write('\n')
-        elif send_log == 'tcp':
-            # init socket
-            sock = None
-            # create socket ?
-            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            # Set timeout
-            timeout=5
-            sock.settimeout(timeout)
-            # Set dest address
-            serverAddress = (dest_ip, dest_port)
-            # Connect
-            sock.connect(serverAddress)
-            # print(sock.recv(1024))
-            # Message to send
-            message = convertLog(log_format, mylog).encode()
-            # Send Message
-            sock.sendall(message)
-            # Close socket
-            sock.close()
-        elif send_log == 'udp':
-            sock = None
-            message = convertLog(log_format, mylog).encode()
-            sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            timeout=5
-            sock.settimeout(timeout)
-            sock.sendto(message, (dest_ip, dest_port))
-            sock.close()
+        # if send_type == tcp OR udp, Send log
+        else:
+            send_log(send_type, dest_ip, dest_port, log_format, my_log)
 
-        time.sleep(5)
+        # Sleep
+        time.sleep(sleep_time)
 
 
 if __name__=='__main__':
